8fd06dc41875d98c8e65f73fe3d52f7f6dccc71c
Duc Huy Ly <78007468+duc-huy-ly@users.noreply.github.com>
2026-01-31 01:54:49 +0100

    bugfix : magic numbers & specify global variables for clock

--- Clock/clock.gd
+++ Clock/clock.gd
@@ -2,15 +2,16 @@ extends Node2D
 
 ## The amount of seconds that it takes for the hand of the clock to complete 
 ## one revolution
-var GAME_DURATION_MINUTES : int = 10
+const GAME_DURATION_MINUTES : int = 10
+const SECONDS_IN_MINUTE: int = 60
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass 
 	
 func _on_timer_of_minutes_timeout() -> void:
-	$Face/Minutes.rotate(2* PI / GAME_DURATION_MINUTES )
+	$Face/Minutes.rotate(2*PI/GAME_DURATION_MINUTES)
 
 func _on_timer_of_seconds_timeout() -> void:
-	$Face/Seconds.rotate(PI/24)
+	$Face/Seconds.rotate(2*PI/SECONDS_IN_MINUTE)
 	
