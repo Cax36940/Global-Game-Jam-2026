adc5b88b85786a0e50778f7d0fb4abecc1a09959
Constantin <cristoval.lettiere@proton.me>
2026-01-31 15:37:52 +0100

    Scaffolding for proof of concept page turn

--- Notebook/true_notebook.gd
+++ Notebook/true_notebook.gd
@@ -1,12 +1,30 @@
 extends Node2D
 
 var cur = 0
-
 var pages = [0,1,2,3,4,5,6]
+
+var sprites: Array
 
 func _ready() -> void:
 	visible = false
 	SignalBus.Notebook_show.connect(signal_handler)
+	
+	# TODO: put into resource tree
+	for i in range(len(pages)*2):
+		var node = Sprite2D.new()
+		var gradient = Gradient.new()
+		gradient.colors = [
+			Color(float(i)/len(pages), 0, float(i)/len(pages)),
+			Color(float(i)/len(pages), 0, float(i)/len(pages))
+		]
+		var texture = GradientTexture2D.new()
+		texture.width = 64
+		texture.height = 64
+		texture.gradient = gradient
+		node.texture = texture
+		node.visible = false
+		add_child(node)
+		sprites.append(node)
 
 	$Previous.pressed.connect(previous)
 	$Next.pressed.connect(next)
@@ -36,8 +54,9 @@ func previous():
 
 func pages_update():
 	$Textbox.text = "cur = {cur}".format($".")
-	$LeftPage.text = "page" + str(2*cur + 1)
-	$RightPage.text = "page" + str(2*cur + 2)
+	var left = sprites[cur]
+	left.visible = true
+	#left.position = Vector2(400, 300)
 
 func _process(delta: float) -> void:
 	pass
