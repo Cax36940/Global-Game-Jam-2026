8381e5e8954cddea38782d6b49760c9f4004af90
Constantin <cristoval.lettiere@proton.me>
2026-01-31 19:43:12 +0100

    Implement pause menu
    Only starts up after the main menu

---
+++ menu.gd
@@ -0,0 +1,14 @@
+# Contains the logic to coordinate main menu and pause menu
+extends Control
+
+func _ready():
+	$PauseMenu.process_mode = Node.PROCESS_MODE_DISABLED
+	$MainMenu.connect("start_game", _on_start_game)
+
+func _on_start_game():
+	print("hi")
+	$MainMenu.hide()
+	$MainMenu.process_mode = Node.PROCESS_MODE_DISABLED
+	$PauseMenu.process_mode = Node.PROCESS_MODE_ALWAYS
+	
+	get_tree().paused = false
