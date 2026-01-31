24b63175607d33c55e390441e48f0718c02dbf12
Constantin <cristoval.lettiere@proton.me>
2026-01-31 16:08:07 +0100

    Notebook: Add Sprites node

--- Notebook/true_notebook.gd
+++ Notebook/true_notebook.gd
@@ -1,35 +1,17 @@
 extends Node2D
 
 var cur = 0
-var pages = [0,1,2,3,4,5]
+var pages = [0,1,2]
 
 var sprites: Array
 
 func _ready() -> void:
-	visible = false
+	sprites = $Sprites.get_children()
 	SignalBus.Notebook_show.connect(signal_handler)
-	
-	# TODO: put into resource tree
-	for i in range(len(pages)*2):
-		var node = Sprite2D.new()
-		var gradient = Gradient.new()
-		gradient.colors = [
-			Color(float(i)/len(pages), 0, float(i)/len(pages)),
-			Color(float(i)/len(pages), 0, float(i)/len(pages))
-		]
-		var texture = GradientTexture2D.new()
-		texture.width = 100
-		texture.height = 141
-		texture.gradient = gradient
-		node.texture = texture
-		node.visible = false
-		add_child(node)
-		sprites.append(node)
 
 	$Previous.pressed.connect(previous)
 	$Next.pressed.connect(next)
 	pages_update()
-
 
 func signal_handler(value : int) -> void :
 	if value == 0:
@@ -66,7 +48,9 @@ func put_right(sprite):
 	sprite.visible = true
 
 func pages_update():
-	$Textbox.text = "cur = {cur}".format($".")
+	var left = sprites[2*cur]
+	var right = sprites[2*cur+1]
+	$Textbox.text = str(left) + " " + str(right)
 	pages_reset()
-	put_left(sprites[2*cur])
-	put_right(sprites[2*cur+1])
+	put_left(left)
+	put_right(right)
