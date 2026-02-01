460b703ea287bd4eb343501f938b479340bb320d
Constantin <cristoval.lettiere@proton.me>
2026-02-01 12:00:36 +0100

    add correct signal on timeout

--- Clock/clock.gd
+++ Clock/clock.gd
@@ -23,4 +23,4 @@ func _process(_nope) -> void:
 	$Face/Minutes.look_at($Face/Minutes.offset + 10000*Vector2(cos(d),sin(d)))
 	$Face/Seconds.look_at($Face/Seconds.offset + 10000*Vector2(cos(12*d),sin(12*d)))
 	if d > 0.1:# REMETTRE 2*PI !!! ------------------------------------------------------------------------------------------------------------------------------------------------------------------
-		SignalBus.End_game.emit()
+		SignalBus.gameover.emit()
