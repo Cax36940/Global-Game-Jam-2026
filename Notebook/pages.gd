1f4155db39b355d588def651a7fd80fab5bf8324
Baptou-33 <baptiste.dureau@outlook.fr>
2026-02-01 20:31:16 +0100

    swap error notebook patched

--- Notebook/pages.gd
+++ Notebook/pages.gd
@@ -22,19 +22,18 @@ func write_constrain():
 		masks.scale = Vector2(0.5, 0.5)
 		page1.add_constrain(masks)
 		
-	var page2 : Page = add_page()
-	for eye_index in Global.invalid_eye:
-		var eyes : Eyes = eyes_scene.instantiate()
-		eyes.set_eyes(eye_index)
-		page2.add_constrain(eyes)
-		
 	var page3 : Page = add_page()
 	for color_index in Global.invalid_color:
 		var color : Color = Global.color_list[color_index]
 		var color_point : ColorPoint = color_point_scene.instantiate()
 		color_point.set_color.call_deferred(color)
-
 		page3.add_constrain(color_point)
+
+	var page2 : Page = add_page()
+	for eye_index in Global.invalid_eye:
+		var eyes : Eyes = eyes_scene.instantiate()
+		eyes.set_eyes(eye_index)
+		page2.add_constrain(eyes)
 		
 	SignalBus.update_notebook.emit()
 
