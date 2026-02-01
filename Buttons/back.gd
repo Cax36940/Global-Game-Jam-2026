0f7b7fce67601c4f48e6ff1fdb7d7f199fdea263
Baptou-33 <baptiste.dureau@outlook.fr>
2026-02-01 14:02:57 +0100

    push

--- Buttons/back.gd
+++ Buttons/back.gd
@@ -1,8 +1,4 @@
 extends CustomButton
-
-func on_mouse_overing():
-	modulate = Color(0.5, 0.5, 0.5, 0.5)
 	
 func on_button_pressed():
 	SignalBus.checklist_show.emit(1)
-	$"../../Mini_Checklist".visible = true
--- refuse.gd
+++ refuse.gd
@@ -9,7 +9,6 @@ func _process(_delta: float) -> void:
 		sprite.frame = 1
 	else :
 		sprite.frame = 0
-	if Input.is_action_pressed("ui_left"):
-		if not Global.Triple_show:
+	if Input.is_action_pressed("ui_left") and not Global.Triple_show:
 			SignalBus.reject_guest.emit()
 			sprite.frame = 1
