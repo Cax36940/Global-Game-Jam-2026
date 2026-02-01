e18026f837c6ba6196052aca5e9acd2b054b037f
Constantin <cristoval.lettiere@proton.me>
2026-02-01 01:57:53 +0100

    Simplify main.gd by moving display code to gui.gd

--- main.gd
+++ main.gd
@@ -9,37 +9,30 @@ func _ready() -> void:
 	SignalBus.gameover.connect(_on_gameover)
 	SignalBus.mainmenu.connect(_on_main_menu)
 
-	$GUI/Overlay.hide()
-	$GUI/PauseMenu.hide()
 	$GUI/PauseMenu.process_mode = Node.PROCESS_MODE_DISABLED
 
 	# Set up first screen
 	get_tree().paused = true
 	ispaused = true
-	$GUI/MainMenu.display()
+	$GUI.display("MainMenu")
 
 func _on_main_menu() -> void:
 	get_tree().reload_current_scene()
 
 func _on_start_game():
-	$GUI/GameOver.hide()
-	$GUI/MainMenu.hide()
 	$GUI/MainMenu.process_mode = Node.PROCESS_MODE_DISABLED
-	$GUI/PauseMenu.hide()
 	$GUI/PauseMenu.process_mode = Node.PROCESS_MODE_ALWAYS
-	$GUI/Overlay.show()
+	$GUI.display("Overlay")
 	get_tree().paused = false
 
 func _on_pause_game(ispaused: bool):
 	if ispaused:
-		$GUI/PauseMenu.display()
-		$GUI/Overlay.hide()
+		$GUI.display("PauseMenu")
 		get_tree().paused = true
 	else:
-		$GUI/PauseMenu.hide()
-		$GUI/Overlay.show()
+		$GUI.display("Overlay")
 		get_tree().paused = false
 
 func _on_gameover():
 	get_tree().paused = true
-	$GUI/GameOver.display()
+	$GUI.display("GameOver")
