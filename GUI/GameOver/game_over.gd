705481886d1002ab0f04cee8f6001fc610ac329c
Constantin <cristoval.lettiere@proton.me>
2026-02-01 18:58:02 +0100

    Remove try again option because that's too complicated to implement at this point

--- GUI/GameOver/game_over.gd
+++ GUI/GameOver/game_over.gd
@@ -1,11 +1,11 @@
 extends MenuScreen
 
 func _ready():
-	$"VBoxContainer/Try Again".pressed.connect(_on_try_again_button_pressed)
+	$"VBoxContainer/Exit".pressed.connect(_on_exit_button_pressed)
 	$"VBoxContainer/Main Menu".pressed.connect(_on_main_menu_button_pressed)
 
-func _on_try_again_button_pressed():
-	SignalBus.start_game.emit()
+func _on_exit_button_pressed():
+	SignalBus.exit_game.emit()
 
 func _on_main_menu_button_pressed():
 	SignalBus.mainmenu.emit()
